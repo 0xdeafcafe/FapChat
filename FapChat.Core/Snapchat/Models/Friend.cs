5fd9597a92f4fe6de46b0eab268c0d6a05e9b7bd
Alex Reed (Xerax) <alexreed@outlook.com>
2013-11-08 14:14:53 +0000

    renamed paths, added models

---
+++ FapChat.Core/Snapchat/Models/Friend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FapChat.Core.Snapchat.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Friend
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyAttribute("can_see_custom_stories")]
+        public Boolean CanSeeCustomStories { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyAttribute("name")]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyAttribute("display")]
+        public String Display { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyAttribute("type")]
+        public Int32 Type { get; set; }
+    }
+}
---
+++ FapChat.Core/Snapchat/Models/Snap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FapChat.Core.Snapchat.Models
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    public class Snap
+    {
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("id")]
+        public String Id { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("rp")]
+        public String Rp { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("ts")]
+        public Int64 Ts { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("sts")]
+        public Int64 Sts { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("c_id")]
+        public String CId { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("m")]
+        public Int32 M { get; set; }
+
+		/// <summary>
+		/// 
+        /// </summary>
+        [JsonPropertyAttribute("st")]
+        public Int32 St { get; set; }
+    }
+}
