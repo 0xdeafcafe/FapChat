9a0a06201af1372447df2af810be13839d85f5ee
Alex Reed (Xerax) <alexreed@outlook.com>
2013-11-08 20:28:00 +0000

    added navigation helper

---
+++ FapChat.Wp8/Helpers/Navigation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace FapChat.Wp8.Helpers
+{
+    public static class Navigation
+    {
+        /// <summary>
+        /// Targets Navigation can get to
+        /// </summary>
+        public enum NavigationTarget
+        {
+            Capture,
+            Settings,
+            Login
+        }
+
+        /// <summary>
+        /// Generates a Windows Phone Path Url to the target you want to navigate to
+        /// </summary>
+        /// <param name="target">The target to navigate to</param>
+        /// <returns>A string representation of the path</returns>
+        public static string GenerateNavigateUrl(NavigationTarget target)
+        {
+            switch (target)
+            {
+                case NavigationTarget.Capture:
+                    return "/Pages/Authed/Capture.xaml";
+
+                case NavigationTarget.Settings:
+                    return "/Pages/Authed/Settings.xaml";
+
+                case NavigationTarget.Login:
+                default:
+                    return "/Pages/Login.xaml";
+            }
+        }
+
+        /// <summary>
+        /// Generates a Windows Phone Path Uri to the target you want to navigate to
+        /// </summary>
+        /// <param name="target">The target to navigate to</param>
+        /// <returns>A Uri representation of the path</returns>
+        public static Uri GenerateNavigateUri(NavigationTarget target)
+        {
+            return new Uri(GenerateNavigateUrl(target), UriKind.Relative);
+        }
+    }
+}
